fc37de8595de1e9e9c52959acce041b7007b3a91
profyoni <jrobinson@touro.edu>
2017-03-15 18:05:42 -0400

    first commit

--- Gui1/src/main/java/mco364/EntryPoint.java
+++ Gui1/src/main/java/mco364/EntryPoint.java
@@ -1,20 +1,90 @@
 package mco364;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class CalcPanel extends JPanel {
+
+    public CalcPanel() {
+        setLayout(new GridLayout(4, 3, 5, 5));
+        //A closure is a block of code that can be referenced (and passed around) with access to the variables of the enclosing scope.
+        // http://stackoverflow.com/questions/5443510/closure-in-java-7
+        for (int i = 0; i < 10; i++) { // i is auotmatic variable 
+            // that gets created on this line and is destoyed when 
+            // block / method exits
+            final int ii = i;
+            JButton b = new JButton(i + "");
+            add(b);
+            b.addActionListener(new ActionListener() {
+                // final int ouputVal;
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    
+                    System.out.println(ii); // why must i be final
+                }
+            });
+        }
+    }
+}
+
+class Calc extends JFrame {
+    
+    public Calc() {
+        super("Calculator");
+
+        //app.setLayout(new BorderLayout()); // not needed since BorderLayout is default for JFRame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // equiv to
+//        addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent we) {
+//                boolean unsavedChanges = false;
+//                if (unsavedChanges == true) { // dirtyBit : https://en.wikipedia.org/wiki/Dirty_bit
+//
+//                }
+//                System.exit(0);
+//            }
+//
+//        });
+        setSize(400, 200);
+
+//        JMenuBar menubar = new JMenuBar();
+//        JMenu editMenu = new JMenu("Edit");
+//        editMenu.add(new JMenuItem("Clear"));
+//        editMenu.add(new JMenuItem("Hi"));
+//        editMenu.add(new JMenuItem("Bye"));
+//        editMenu.addSeparator();
+//        
+//        JMenuItem exitMenuItem = new JMenuItem("Exit");
+//        exitMenuItem.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent ae) {
+//                System.exit(0);
+//            }
+//        });
+//        editMenu.add(exitMenuItem);
+//        menubar.add(editMenu);
+        JTextPane textPanel = new JTextPane();
+        textPanel.setContentType("text/html");
+        textPanel.setText("<strong>1234</strong><br>QWE");
+        
+        JPanel mainPanel2 = new CalcPanel(); // lightweight -- implement in pure Java
+        
+        add(mainPanel2, BorderLayout.CENTER);
+        
+        JLabel statusBar = new JLabel("App has started...");
+        add(statusBar, BorderLayout.SOUTH);
+        
+        add(textPanel, BorderLayout.NORTH);
+        
+        setVisible(true);
+        
+    }
+}
 
 public class EntryPoint {
-
+    
     public static void main(String[] args) {
         // JFrame = windowed app
         // // center of JFrame
@@ -28,45 +98,8 @@ public class EntryPoint {
         // -- event handler
         // -- executable jar with icon
 
-        JFrame app = new JFrame("Calculator");
-        app.setLayout(new GridLayout(4, 3, 5, 5));
-        //app.setLayout(new BorderLayout()); // not needed since BorderLayout is default for JFRame
-        // app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // equiv to
-        app.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent we) {
-                boolean unsavedChanges = false;
-                if (unsavedChanges == true) { // dirtyBit : https://en.wikipedia.org/wiki/Dirty_bit
-
-                }
-                System.exit(0);
-            }
-
-        });
-        app.setSize(400, 200);
-
-        JPanel mainPanel = new JPanel(); // lightweight -- implement in pure Java
-        app.add(mainPanel, BorderLayout.CENTER);
-        app.add(new JLabel("App has started..."), BorderLayout.SOUTH);
-
-        for (int i = 0; i < 10; i++) { // i is auotmatic variable 
-            // that gets created on this line and is destoyed when 
-            // block / method exits
-            final int ii = i;
-            JButton b = new JButton(i + "");
-            app.add(b);
-            b.addActionListener(new ActionListener() {
-                // final int ouputVal;
-                @Override
-                public void actionPerformed(ActionEvent ae) {
-
-                    System.out.println(ii); // why must i be final
-                }
-            });
-        }
-
-        app.setVisible(true);
+        JFrame app = new Calc();
+        
         System.out.println("End");
     }
 }
